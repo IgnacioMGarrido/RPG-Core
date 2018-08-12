18420b3ee0442340c90c33d984c6c93a4a176f9d
Ignacio Martinez <nama1510@gmail.com>
2018-08-12 21:36:38 +0200

    RPG_02

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Use this for initialization
+    GameObject player;
+	void Start () {
+		if(player == null)
+            player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        this.transform.position = player.transform.position;
+	}
+}
