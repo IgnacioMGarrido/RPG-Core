4636288aa4ed08410939fc9027c26692fc07236d
Ignacio Martinez <nama1510@gmail.com>
2018-08-13 16:19:29 +0200

    RPG_08
    Fixed Movement bug

--- Assets/Camera & UI/CursorAffordance.cs
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -14,7 +14,7 @@ public class CursorAffordance : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
         switch (cameraRaycaster.layerHit)
         {
             case Layer.Walkable:
--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,12 +5,18 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+
     [SerializeField] float walkMoveStopRadius = .2f;
 
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+
+
+    bool m_Jump = false;
+    bool isInDirectMode = false; //TODO: Consider make it Static later
+
+
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -18,11 +24,31 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
     //TODO: Fix conflict between click movement and WASD movement
-
+    private void Update()
+    {
+        /*  if (!m_Jump)
+          {
+              m_Jump = Input.GetButtonDown("Jump");
+          }*/
+        //TODO: Let the user map later or add to menu
+        if (Input.GetKeyDown(KeyCode.G)) 
+        {
+            isInDirectMode = !isInDirectMode;
+        }
+    }
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
         bool crouch = Input.GetKey(KeyCode.C);
+
+        if (isInDirectMode == true)
+            ProcessDirectMovement(crouch);
+        else
+            ProcessIndirectMouseMovement(crouch);
+    }
+
+    private void ProcessIndirectMouseMovement(bool crouch)
+    {
         if (Input.GetMouseButton(0))
         {
             print("Cursor raycast hit" + cameraRaycaster.layerHit);//hit.collider.gameObject.name.ToString());
@@ -49,6 +75,18 @@ public class PlayerMovement : MonoBehaviour
         {
             m_Character.Move(Vector3.zero, crouch, false);
         }
+    }
+
+    private void ProcessDirectMovement(bool crouch)
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+
+        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+        m_Character.Move(m_Move, crouch, m_Jump);
 
     }
 }
