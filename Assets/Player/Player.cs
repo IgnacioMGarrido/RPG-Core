34a692e8a4133cdb9f94ca98df2c7c84a85eb594
Ignacio Martinez <nama1510@gmail.com>
2018-08-14 16:55:37 +0200

    RPG_14
    Added Health Bar

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField] float maxHealthPoints = 100f;
+    float currenthealthPoints = 100f;
+
+    public float healthAsPercentage
+    {
+        get
+        {
+            return currenthealthPoints / (float) maxHealthPoints;
+        }
+
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
